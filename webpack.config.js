ec98f2ef58cfe20c744e3daf8e75e141e46a3fae
Oscar <oscareascencioalva@gmail.com>
2021-06-23 23:45:55 -0500

    Diseño

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+
+    module: {
+        rules: [{
+                test: /\.html$/,
+                loader: 'html-loader',
+
+                options: {
+                    sources: false,
+                    minimize: false
+                }
+            },
+            {
+                test: /\.css$/i,
+                exclude: /style.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /style.css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: 'file-loader',
+                }, ],
+            },
+
+
+        ]
+
+    },
+
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html',
+            inject: 'body'
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/medicos.html',
+            filename: './medicos.html',
+            inject: 'body'
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/añadir-medico.html',
+            filename: './añadir-medico.html',
+            inject: 'body'
+        }),
+        new HtmlWebPackPlugin({
+            template: './src/añadir-paciente.html',
+            filename: './añadir-paciente.html',
+            inject: 'body'
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.css',
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets/' },
+            ],
+        }),
+
+    ]
+}
